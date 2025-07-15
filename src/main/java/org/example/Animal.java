e8d851c73aa24af39b2e1211538f4eb39c6f60cb
yuliiabezkorovaina <yuliya.bezkorovayna@gmail.com>
2025-07-15 15:02:25 +0200

    Bonusaufgabe ist fertig + Lombok

---
+++ src/main/java/org/example/Animal.java
@@ -0,0 +1,41 @@
+package org.example;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Month;
+import java.time.MonthDay;
+import java.time.temporal.ChronoUnit;
+
+@Data
+@NoArgsConstructor
+public class Animal {
+
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        animal.name = "Miura";
+        animal.dateBirth = LocalDate.of(2020, 07, 17);
+        System.out.println(animal);
+       animal.zumNaecstenGeburstag();
+
+    }
+
+    String name;
+    LocalDate  dateBirth;
+
+    public void zumNaecstenGeburstag(){
+        MonthDay geburtstag = MonthDay.of(dateBirth.getMonth(), dateBirth.getDayOfMonth());
+        LocalDate naechsterGeburtstag = geburtstag.atYear(LocalDate.now().getYear());
+        if (!naechsterGeburtstag.isAfter(LocalDate.now())) {
+            naechsterGeburtstag = naechsterGeburtstag.plusYears(1);
+        }
+
+
+        long tageBisGeburtstag = ChronoUnit.DAYS.between(LocalDate.now(), naechsterGeburtstag);
+
+
+        System.out.println("Noch " + tageBisGeburtstag + " Tage bis zum nächsten Geburtstag des Tieres.");
+    }
+}
